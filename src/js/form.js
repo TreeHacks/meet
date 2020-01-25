8670a440bf7046eeafff1d854507500e3806c94e
Lucas Pauker <lucas.pauker@gmail.com>
2020-01-24 17:13:13 -0800

    More updates

--- src/js/form.js
+++ src/js/form.js
@@ -5,28 +5,23 @@ import API from "@aws-amplify/api";
 const schema = {
   title: "Profile",
   type: "object",
-  required: ["first_name", "last_name"],
+  required: [],
   properties: {
-    first_name: {type: "string", title: "First name"},
-    last_name: {type: "string", title: "Last name"},
     verticals: {
       "type": "array",
       "uniqueItems": true,
       "items": {"type": "string", enum:["education", "medical access", "voice assistance", "iot", "ar/vr", "geospatial"] }
     },
-    q1: {type: "string", title: "Challenge ideas"},
-    q_slack: {type: "string", title: "Slack"},
-    privacy: {type: "boolean", title: "Yes! Show my profile and allow other hackers to contact me.", default: false}
+    idea: {type: "string", title: "Challenge ideas"},
+    showProfile: {type: "boolean", title: "Yes! Show my profile and allow other hackers to contact me.", default: false}
   }
 };
 
 const uiSchema = {
-  first_name: {"ui:placeholder": "First name"},
-  last_name: {"ui:placeholder": "Last name"},
   verticals: {
     "ui:widget": "checkboxes"
   },
-  q1: {"ui:widget": "textarea", "ui:placeholder": "Challenge ideas (150 words max)"},
+  idea: {"ui:widget": "textarea", "ui:placeholder": "Pitch an idea and/or tell other hackers about yourself! (150 words max)"}
 }
 
 const log = (type) => console.log.bind(console, type);
@@ -38,11 +33,6 @@ class MeetForm extends React.Component {
   }
 
   async componentDidMount() {
-    //let p = new Promise(resolve => {
-    //  if (this.props.user != null) resolve()
-    //});
-    //await p;
-    //console.log(this.props.user == null);
     const user_info = await API.get("treehacks", `/users/${this.props.user.username}`, {});
     console.log(user_info);
     const meet_info = await API.get("treehacks", `/users/${this.props.user.username}/forms/meet_info`, {});

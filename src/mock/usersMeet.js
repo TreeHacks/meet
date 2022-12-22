284b31f16c691b9d3b5a2e0dbebad4dbba575ef0
GashonHussein <gashon96@gmail.com>
2022-12-21 19:44:16 -0700

    refactor: additional mock data schematics

--- src/mock/usersMeet.js
+++ src/mock/usersMeet.js
@@ -6,15 +6,29 @@ const occupations = [
 ];
 const interests = ["music", "art", "design", "business"];
 const firstNames = ["John", "Jane", "Bob", "Alice"];
+const lastNames = ["Doe", "Robertson", "King", "Price"];
+const skills = ["AI", "Data Mining", "NLP", "Web Development", "IOS", "Android", "Pitching", "Marketing", "Design", "AR/VR", "Game Development", "Systems"];
+const commitment = ["High", "Medium", "Low"];
 
 const getRandom = (arr) =>
   arr[Math.floor(Math.random() * arr.length)];
 
+const randomSet = (arr, maxSize) => [...new Array(Math.floor(Math.random() * maxSize))].map(() => getRandom(arr));
+
 const getMeetInfo = () => ({
-  showProfile: Math.random() > 0.15,
+  showProfile: Math.random() > 0.10,
   profileDesc: `I am a ${getRandom(occupations)}`,
-  verticals: [getRandom(interests), getRandom(interests)],
+  verticals: randomSet(interests, 3),
   first_name: getRandom(firstNames),
+  last_name: getRandom(lastNames),
+  isMentor: Math.random() > 0.75,
+  linkedinLink: "https://www.linkedin.com/in/williamhgates/",
+  devpostLink: "https://devpost.com/",
+  githubLink: "https://github.com/",
+  timezoneOffset: "GMT -0500",
+  skills: randomSet(skills, 5),
+  commitment: getRandom(commitment),
+  idea: "No idea yet",
   pronouns: "they/them",
   profilePicture:
     "https://upload.wikimedia.org/wikipedia/en/d/d7/Random_person_image.png",

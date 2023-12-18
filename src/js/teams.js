22a86d5b25b9c362e52be13214f262b68aae5c56
Ananya Gupta <93958307+guananya@users.noreply.github.com>
2023-12-18 03:59:10 -0800

    get output correctly getLists

--- src/js/teams.js
+++ src/js/teams.js
@@ -86,7 +86,7 @@ class MeetForm extends React.Component {
     console.log("username", this.props.user.username);
     console.log("email", this.props.user.attributes.email);
 
-    var {pending_list, approved_list} = await this.getLists(this.props.user.username);
+    var { pendLis: pending_list, apprLis: approved_list } = await this.getLists(this.props.user.username);
     console.log("from helper", pending_list);
     console.log("from helper", approved_list);
     
@@ -110,8 +110,8 @@ class MeetForm extends React.Component {
     console.log("doing add");
     console.log(caller);
     console.log(called);
-    var { calledPending, calledApproved} = await this.getLists(called);
-    var { callerPending, callerApproved} = await this.getLists(caller); 
+    var { pendLis: calledPending, apprLis: calledApproved } = await this.getLists(called);
+    var { pendLis: callerPending, apprLis: callerApproved } = await this.getLists(caller); 
     var pendingList = [];
   }
 
